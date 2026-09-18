fb045793000bc9f83dcde9b95ff474a3956613f3
Loc Ton That <thatlocthpy261@gmail.com>
2026-09-19 00:06:20 +0700

    Add files via upload

---
+++ BAITAP/BAITAP#5/BAI01/ChuyenXe.cs
@@ -0,0 +1,32 @@
+﻿namespace Module03
+{
+    public class ChuyenXe
+    {
+        public string MaSoChuyen { get; set; }
+        public string HoTenTaiXe { get; set; }
+        public string SoXe { get; set; }
+        public double DoanhThu { get; set; }
+        protected ChuyenXe(string maSoChuyen, string hoTenTaiXe,
+                        string soXe, double doanhThu)
+        {
+            MaSoChuyen = maSoChuyen;
+            HoTenTaiXe = hoTenTaiXe;
+            SoXe = soXe;
+            DoanhThu = doanhThu;
+        }
+
+        public virtual double TinhDoanhThu()
+        {
+            return DoanhThu;
+        }
+
+        public override string ToString()
+        {
+            return $"Mã chuyến: {MaSoChuyen}, " +
+                   $"Tài xế: {HoTenTaiXe}, " +
+                   $"Số xe: {SoXe}, " +
+                   $"Doanh thu: {DoanhThu}";
+        }
+    }
+
+}
