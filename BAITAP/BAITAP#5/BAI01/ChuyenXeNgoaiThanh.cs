a5fd854338f84420c4e1c87b882461ad7a9f7832
Loc Ton That <thatlocthpy261@gmail.com>
2026-09-19 00:26:45 +0700

    Add files via upload

---
+++ BAITAP/BAITAP#5/BAI01/ChuyenXeNgoaiThanh.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Module03
+{
+    public class ChuyenXeNgoaiThanh : ChuyenXe
+    {
+        private string NoiDen;
+        private int SoNgay;
+
+        public ChuyenXeNgoaiThanh(
+            string maSoChuyen,
+            string hoTenTaiXe,
+            string soXe,
+            double doanhThu,
+            string noiDen,
+            int soNgay)
+            : base(maSoChuyen, hoTenTaiXe, soXe, doanhThu)
+        {
+            NoiDen = noiDen;
+            SoNgay = soNgay;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                   $", Nơi đến: {NoiDen}" +
+                   $", Số ngày: {SoNgay}";
+        }
+    }
+}
