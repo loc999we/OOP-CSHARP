328982e507f7cdf9c4b92bc8ad6f5a3c77fe69aa
Loc Ton That <thatlocthpy261@gmail.com>
2026-09-19 11:55:29 +0700

    Add files via upload

---
+++ BAITAP/BAITAP#5/BAI04/SachGiaoKhoa.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Bai2Module3
+{
+    public class SachGiaoKhoa : Sach
+    {
+        public string TinhTrang { get; set; }
+        public override string LoaiSach
+        {
+            get
+            {
+                return "Giáo khoa";
+            }
+        }
+
+        public SachGiaoKhoa(
+            string maSach,
+            DateTime ngayNhap,
+            double donGia,
+            int soLuong,
+            string nhaXuatBan,
+            string tinhTrang)
+            : base(maSach, ngayNhap, donGia, soLuong, nhaXuatBan)
+        {
+            TinhTrang = tinhTrang;
+        }
+
+        public override double TinhThanhTien()
+        {
+            if (TinhTrang.Equals("mới", StringComparison.OrdinalIgnoreCase))
+                return SoLuong * DonGia;
+
+            return SoLuong * DonGia * 0.5;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" | Tình trạng: {TinhTrang}";
+        }
+    }
+}
